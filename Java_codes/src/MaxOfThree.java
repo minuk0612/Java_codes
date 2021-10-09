9746527f3c9c06bbfe564245874dc22ba041caad
minu kumari <minusingh2250@gmail.com>
2021-10-09 19:46:19 +0530

    2nd commit

--- Java_codes/src/MaxOfThree.java
+++ Java_codes/src/MaxOfThree.java
@@ -1,4 +1,31 @@
+import java.util.Scanner;
 
 public class MaxOfThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter first number:");
+		int num1=sc.nextInt();
+		System.out.println("Enter second number:");
+		int num2=sc.nextInt();
+		System.out.println("Enter third number");
+		int num3=sc.nextInt();
+		if(num1>num2) {
+			if(num1>num3)
+				System.out.println("The largest number is:"+ num1);
+			else
+				System.out.println("The largest number is:"+ num3);
+		}
+		else if(num2>num1) {
+			if(num2>num3) {
+				System.out.println("The largest number is:"+ num2);
+			}
+			else
+			System.out.println("The largest number is:"+ num3);
+		}
+		
+	}
 
 }
